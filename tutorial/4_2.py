896f522eb5fc9c5ca5baead7e61bbc6095512d60
popohato <64397377+popohato@users.noreply.github.com>
2021-09-25 07:30:42 +0900

    make tutorial folder

--- tutorial/4_2.py
+++ tutorial/4_2.py
@@ -3,9 +3,15 @@ from typing import List
 
 words: List(str) = ["cat", "window", "defenestrate"]
 for w in words:
-    print(w, len(w))
+    print(w, len(w)) # =>
+    
+# cat 3
+# window 6
+# defenestrate 12
 
 # for構文。
+# for <変数> in 定数:
+#    処理()
 
 # len()はPythonの組み込み関数で、文字列、タプル、リスト、また辞書などのアイテム数を
 # 返す関数。文字列の場合は文字数が返される。
